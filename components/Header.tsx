d26e035a0c10feaedc6a385e9401fb8742aab09f
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-05-06 22:34:59 -0400

    Update: Header and Skills
    Update header margin for md. screen and styling of skills section. Additionally, added a tooltip to each skills.

--- components/Header.tsx
+++ components/Header.tsx
@@ -61,7 +61,7 @@ export default function Header({ socials }: Props) {
                     fgColor="gray" 
                     bgColor="transparent"
                 />
-                <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
+                <p className='uppercase hidden md:inline-flex text-sm text-gray-400 md:mr-6'>
                     Get In Touch
                 </p>
             </motion.div>
--- components/Skill.tsx
+++ components/Skill.tsx
@@ -20,11 +20,11 @@ function Skill({ skill }: Props) {
             whileInView={{ opacity: 1, x: 0 }}
             src={urlFor(skill?.image).url()}
             className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 
-            xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+            xl:w-28 xl:h-28 filter group-hover:grayscale transition duration-300 ease-in-out'
         />
         <div className='absolute opacity-0 group-hover:opacity-80 transition duration-500 ease-in-out
-        group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
-            <div className='flex items-center justify-center h-full'>
+        group-hover:bg-yellow-300 h-24 w-24 md:w-28 md:h-28 xl:w-28 xl:h-28 rounded-full z-0'>
+            <div className='flex items-center justify-center h-full' title={skill.title} >
                 <p className='text-3xl font-bold text-black opacity-100'>{skill.progress}%</p>
             </div>
         </div>
