9091a14a1b8fc695246956418ec5bc0c14f54100
Gabriel Diniz <55710093+GabeDiniz@users.noreply.github.com>
2023-05-23 16:29:59 -0400

    Update: style change
    Change contact, skills, and hero styling

--- components/About.tsx
+++ components/About.tsx
@@ -14,7 +14,7 @@ export default function About({pageInfo}: Props) {
         whileInView={{ opacity: 1 }}
         transition={{ duration: 1.5 }}
         className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
+        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About Me</h3>
         
         <motion.img 
             className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
@@ -34,7 +34,7 @@ export default function About({pageInfo}: Props) {
         />
 
         <div className='space-y-10 px-0 md:px-10'>
-            <h4 className='text-4xl font-semibold'>Nice to meet you</h4>
+            <h4 className='text-4xl font-semibold'>Nice to meet you!</h4>
             <p className='lg:text-lg md:text-sm xs:text-sm'>{pageInfo?.backgroundInformation}</p>
         </div>
     </motion.div>
--- components/Skill.tsx
+++ components/Skill.tsx
@@ -19,11 +19,11 @@ function Skill({ skill }: Props) {
             transition={{ duration: 1 }}
             whileInView={{ opacity: 1, x: 0 }}
             src={urlFor(skill?.image).url()}
-            className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 
-            xl:w-28 xl:h-28 filter group-hover:grayscale transition duration-300 ease-in-out'
+            className='rounded-full border border-gray-500 object-cover w-24 h-24 p-1 filter group-hover:grayscale transition duration-300 ease-in-out'
         />
+        {/* Media Queries: md:w-28 md:h-28 xl:w-28 xl:h-28 */}
         <div className='absolute opacity-0 group-hover:opacity-80 transition duration-500 ease-in-out
-        group-hover:bg-yellow-300 h-24 w-24 md:w-28 md:h-28 xl:w-28 xl:h-28 rounded-full z-0'>
+        group-hover:bg-yellow-300 h-24 w-24 rounded-full z-0'>
             <div className='flex items-center justify-center h-full' title={skill.title} >
                 <p className='text-3xl font-bold text-black opacity-100'>{skill.progress}%</p>
             </div>
