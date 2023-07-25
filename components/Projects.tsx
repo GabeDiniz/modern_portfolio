955fbdb33ebe6864336c4f03cfc08b615beba4ab
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-07-24 21:52:27 -0400

    Update: projects L2B WIP

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -20,6 +20,9 @@ export default function Projects({ projects }: Props) {
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
             Projects
         </h3>
+        <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>
+                Click on the icon to view the build
+            </h3>
         <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory
             z-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-highlight/80 mb-2'>
             {projects?.map((project, i) => (
