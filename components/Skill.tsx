473fd51f527549985035870f7e798ed4fdb8278c
gdiniz <gabriel.sundiniz@gmail.com>
2023-08-02 14:29:43 -0400

    Fix: media queries for Skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -19,7 +19,7 @@ function Skill({ skill }: Props) {
             transition={{ duration: 1 }}
             whileInView={{ opacity: 1, x: 0 }}
             src={urlFor(skill?.image).url()}
-            className='rounded-full border border-gray-500 object-cover w-24 h-24 p-1 filter group-hover:grayscale transition duration-300 ease-in-out'
+            className='rounded-full border border-gray-500 object-cover w-20 h-20 md:w-20 md:h-20 xl:w-24 xl:h-24 p-1 filter group-hover:grayscale transition duration-300 ease-in-out'
         />
         {/* Media Queries: md:w-28 md:h-28 xl:w-28 xl:h-28 */}
         <div className='absolute opacity-0 group-hover:opacity-80 transition duration-500 ease-in-out
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -18,14 +18,16 @@ function Skills({ skills }: Props) {
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }} 
-            className='h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px]
-                        xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'
+            className='h-screen flex flex-col text-center md:text-left max-w-[2000px]
+                        xl:px-10 min-h-screen justify-center mx-auto items-center'
         >
-            <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+            <h3 className='uppercase mb-2 tracking-[20px] text-gray-500 text-2xl'>
                 Skills
             </h3>
+            {/* absolute top-24 
+absolute top-36 */}
 
-            <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>
+            <h3 className='uppercase mb-6 2xl:mb-14 tracking-[3px] text-gray-500 text-sm'>
                 Hover over a skill for proficiency
             </h3>
 
