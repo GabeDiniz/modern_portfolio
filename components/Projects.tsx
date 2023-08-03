51d47ead51a4f63d306f4c32103d0cfb63712ef2
gdiniz <gabriel.sundiniz@gmail.com>
2023-08-03 15:27:40 -0400

    WIP: responsive projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -14,23 +14,23 @@ export default function Projects({ projects }: Props) {
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 1.5 }} 
-        className='h-screen flex relative flex-col text-left md:flex-row max-w-full justify-evenly 
+        className='h-screen flex relative flex-col text-left max-w-full
             mx-auto items-center z-0'
     >
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+        <h3 className='uppercase tracking-[20px] text-gray-500 text-2xl'>
             Projects
         </h3>
-        <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>
+        <h3 className='uppercase tracking-[3px] text-gray-500 text-sm debug'>
                 Click on the icon to view the build
             </h3>
-        <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory
-            z-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-highlight/80 mb-2'>
+        <div className='relative w-full flex content-start justify-start overflow-x-scroll overflow-y-hidden snap-x snap-mandatory
+            z-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-highlight/80 debug'>
             {projects?.map((project, i) => (
                 <div
                     key={i}
                     className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center
-                    justify-center p-20 md:p-44 h-screen'>
-                    
+                    justify-center p-20 md:p-44 h-screen'
+                >
                     <motion.img
                         initial={{ 
                             opacity: 0,
@@ -40,9 +40,9 @@ export default function Projects({ projects }: Props) {
                         transition={{ duration: 1.2 }}  
                         viewport={{ once: true }}
                             src={urlFor(project?.image).url()}
-                        className='w-[16rem] bg-cover'
+                        className='w-[16rem] bg-cover debug'
                     />
-                    <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
+                    <div className='space-y-10 px-0 md:px-10 max-w-6xl debug'>
                         <h4 className='text-4xl font-semibold text-center'>
                             <span className=''>
                                 Project {i + 1} of {projects.length}:
