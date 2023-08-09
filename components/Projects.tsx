78b02f9d6fe45b024072ed0dab1ae73765880940
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-08-08 21:06:26 -0400

    Update: project text and img size

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -41,7 +41,7 @@ export default function Projects({ projects }: Props) {
                         transition={{ duration: 1.2 }}  
                         viewport={{ once: true }}
                             src={urlFor(project?.image).url()}
-                        className='w-[16rem] bg-cover'
+                        className='w-[12rem] bg-cover'
                     />
                     <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
                         <h4 className='text-4xl font-semibold text-center'>
@@ -63,7 +63,7 @@ export default function Projects({ projects }: Props) {
                             ))}
                         </div>
                     
-                        <p className='text-lg text-center md:text-left'>
+                        <p className='text-md text-center md:text-left'>
                             {project?.summary}
                         </p>
                     </div>
