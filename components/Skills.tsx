5e502b73228f3ef8205b9de0bc2fa7b65b7b3417
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-08-12 18:07:10 -0400

    Update: skills positioning

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -19,9 +19,9 @@ function Skills({ skills }: Props) {
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }} 
             className='h-screen flex flex-col text-center md:text-left max-w-[2000px]
-                        xl:px-10 min-h-screen justify-center mx-auto items-center'
+                        xl:px-10 min-h-screen mx-auto items-center'
         >
-            <h3 className='uppercase mb-2 tracking-[20px] text-gray-500 text-2xl'>
+            <h3 className='uppercase mt-28 mb-2 tracking-[20px] text-gray-500 text-2xl'>
                 Skills
             </h3>
             {/* absolute top-24 
