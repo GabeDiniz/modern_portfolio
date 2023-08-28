b26a19ab11a39581960fa835e7837c922384a261
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-08-28 18:51:06 -0400

    Resolve: responsive experience section

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -17,10 +17,11 @@ export default function WorkExperience({ experiences }: Props) {
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 1.5 }} 
-        className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full 
-            px-10 justify-evenly mx-auto items-center'
+        className='h-screen flex relative overflow-hidden flex-col text-left max-w-full 
+            px-10 justify-center mx-auto items-center'
     >
-        <h3 className='absolute xl:top-24 md:top-20 uppercase tracking-[20px] text-gray-500 text-2xl'>
+      {/* absolute xl:top-24 md:top-20 */}
+        <h3 className='uppercase tracking-[20px] text-gray-500 text-2xl'>
             Experience
         </h3>
         <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar-thin 
