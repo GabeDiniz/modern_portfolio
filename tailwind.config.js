6a80bbd51a2b3c1e31c6aa35f709cac13089b81f
gdiniz <gabriel.sundiniz@gmail.com>
2023-11-04 15:41:03 -0400

    format: tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,24 +7,19 @@ module.exports = {
   theme: {
     screens: {
       xs: "390px",
+
       sm: "640px",
-      // => @media (min-width: 640px) { ... }
 
       md: "768px",
-      // => @media (min-width: 768px) { ... }
 
       lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
 
       xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
 
       "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
+
       shorter: { raw: "(min-height: 690px)" },
-      // => @media (min-height: 800px) { ... }
       short: { raw: "(min-height: 850px)" },
-      // => @media (min-height: 800px) { ... }
     },
     extend: {
       backgroundImage: {
