136d7e1f40edc12c31d4624b0396636a10a7cd75
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-12-11 20:48:59 -0500

    Update: contact me responsive styling

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -24,43 +24,42 @@ export default function ContactMe({}) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="h-screen flex relative flex-col text-center md:text-left max-w-7xl px-10 
-        mx-auto items-center"
+      className="h-screen flex relative flex-col text-center items-center max-w-7xl px-10 mx-auto "
     >
-      <h3 className="mt-24 short:mt-20 uppercase tracking-[20px] text-gray-500 text-2xl short:text-xl">
+      <h3 className="mt-24 uppercase tracking-[20px] text-gray-500 text-xl sm:text-2xl short:text-xl">
         Contact Me
       </h3>
 
-      <div className="short:mt-6 mt-16 flex flex-col space-y-10">
-        <h4 className="text-3xl short:text-2xl font-semibold text-center">
+      <div className="mt-6 sm:mt-10 short:mt-6 flex flex-col space-y-10">
+        <h4 className="text-lg sm:text-2xl font-semibold text-center">
           I would love to get to know you.{" "}
           <span className="underline decoration-highlight/50">
             Want to chat?
           </span>
         </h4>
 
-        <div className="short:space-y-4 space-y-8">
+        <div className="text-md sm:text-2xl space-y-3 sm:space-y-8 short:space-y-4 ">
           <div className="flex items-center space-x-5 justify-center">
             <PhoneIcon className="text-highlight h-7 w-7 animate-pulse" />
-            <p className="text-2xl short:text-lg">289-380-3053</p>
+            <p className="">289-380-3053</p>
           </div>
 
           <div className="flex items-center space-x-5 justify-center">
             <MapPinIcon className="text-highlight h-7 w-7 animate-pulse" />
-            <p className="text-2xl short:text-lg">Kitchener/Waterloo</p>
+            <p className="">Kitchener/Waterloo</p>
           </div>
 
           <div className="flex items-center space-x-5 justify-center">
             <EnvelopeIcon className="text-highlight h-7 w-7 animate-pulse" />
-            <p className="text-2xl short:text-lg">gabriel.sundiniz@gmail.com</p>
+            <p className="">gabriel.sundiniz@gmail.com</p>
           </div>
         </div>
 
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col space-y-2 w-fit mx-auto"
+          className="flex flex-col space-y-2 mx-auto w-fit"
         >
-          <div className="flex space-x-2">
+          <div className="flex flex-col sm:flex-row sm:space-x-2">
             <input
               {...register("name")}
               className="contactInput"
@@ -89,7 +88,7 @@ export default function ContactMe({}) {
           />
           <button
             type="submit"
-            className="group relative bg-white/80 py-3 rounded-md overflow-hidden text-black font-bold text-lg "
+            className="group relative bg-white/80 py-3 rounded-md overflow-hidden text-black font-bold text-lg"
           >
             <div className="absolute inset-0 w-0 bg-highlight transition-all duration-[300ms] ease-in group-hover:w-full"></div>
             <span className="relative">Send</span>
