418cd107b15414e010ba6f2bdd657ae4591faf15
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-12-19 20:20:27 -0500

    Update: skills schema

--- typings.d.ts
+++ typings.d.ts
@@ -1,68 +1,69 @@
 interface SanityBody {
-    _createdAt: string;
-    _id: string;
-    _rev: string;
-    _updatedAt: string;
+  _createdAt: string;
+  _id: string;
+  _rev: string;
+  _updatedAt: string;
 }
 
 interface Image {
-    _type: "image";
-    asset: {
-        _ref: string;
-        _type: "reference";
-    };
+  _type: "image";
+  asset: {
+    _ref: string;
+    _type: "reference";
+  };
 }
 
 export interface PageInfo extends Sanitybody {
-    _type: "pageInfo";
-    address: string;
-    backgroundInformation: string;
-    email: string;
-    role: string;
-    heroImage: Image;
-    name: string;
-    phoneNumber: string;
-    profilePic: Image;
+  _type: "pageInfo";
+  address: string;
+  backgroundInformation: string;
+  email: string;
+  role: string;
+  heroImage: Image;
+  name: string;
+  phoneNumber: string;
+  profilePic: Image;
 }
 
 export interface Technology extends Sanitybody {
-    _type: "skill";
-    image: Image;
-    progress: number;
-    title: string;
+  _type: "skill";
+  image: Image;
+  progress: number;
+  title: string;
 }
 
 export interface Skill extends Sanitybody {
-    _type: "skill";
-    image: Image;
-    progress: number;
-    title: string;
+  _type: "skill";
+  image: Image;
+  progress: number;
+  title: string;
+  skill: string;
 }
 
 export interface Experience extends Sanitybody {
-    _type: "experience";
-    company: string;
-    companyImage: Image;
-    dateStarted: date;
-    dateEnded: date;
-    isCurrentlyWorkingHere: boolean;
-    jobTitle: string;
-    points: string[];
-    technologies: Technology[];
+  _type: "experience";
+  company: string;
+  companyImage: Image;
+  dateStarted: date;
+  dateEnded: date;
+  isCurrentlyWorkingHere: boolean;
+  jobTitle: string;
+  points: string[];
+  technologies: Technology[];
 }
 
 export interface Project extends Sanitybody {
-    title: string;
-    _type: "project";
-    image: Image;
-    linkToBuild: string;
-    summary: string;
-    technologies: Technology[];
+  title: string;
+  _type: "project";
+  image: Image;
+  linkToBuild: string;
+  summary: string;
+  technologies: Technology[];
 }
 
 export interface Social extends Sanitybody {
-    _id: Key | null | undefined;
-    _type: "social";
-    title: string;
-    url: string;
+  _id: Key | null | undefined;
+  _type: "social";
+  title: string;
+  url: string;
 }
