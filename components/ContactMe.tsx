5be3eb2d55f9300f0abd0480f2ff54d4010b3fcd
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-02-11 19:23:10 -0500

    [remove] embeded form for emialing

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -55,7 +55,7 @@ export default function ContactMe({}) {
           </div>
         </div>
 
-        <form
+        {/* <form
           onSubmit={handleSubmit(onSubmit)}
           className="flex flex-col space-y-2 mx-auto w-fit"
         >
@@ -93,7 +93,7 @@ export default function ContactMe({}) {
             <div className="absolute inset-0 w-0 bg-highlight transition-all duration-[300ms] ease-in group-hover:w-full"></div>
             <span className="relative">Send</span>
           </button>
-        </form>
+        </form> */}
       </div>
     </motion.div>
   );
