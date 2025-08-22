145b4bd6afdaf3ba90a6176bf09f704b0fc94f2d
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-08-21 21:28:01 -0400

    [fix] animation overflow - i actually fixed it for real i think (please)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="overflow-x-hidden">
+      <body>
         <Main />
         <NextScript />
       </body>
